01fe89a0a9042eb1589f888902891ebc39c9a19c
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-02-10 15:08:09 -0300

    Usando event_handler para emitir eventos para a máquina

--- Integrado/Nucleo da Simulacao/include/event_handler/event_handler.hpp
+++ Integrado/Nucleo da Simulacao/include/event_handler/event_handler.hpp
@@ -4,6 +4,16 @@
 #include <machine/machine.hpp>
 #include <events/events.hpp>
 
-//void handle_events(){};
+void handle_events()
+{
+    Core.process_event(Start{});
+    delay(2000);
+    Core.process_event(Timeout{});
+    delay(2000);
+    Core.process_event(Terminate{});
+    delay(2000);
+    Core.process_event(Reset{});
+    delay(500);
+};
 
-#endif 
+#endif
--- Integrado/Nucleo da Simulacao/src/main.cpp
+++ Integrado/Nucleo da Simulacao/src/main.cpp
@@ -1,20 +1,9 @@
-#include <machine/machine.hpp>
-#include <events/events.hpp>
+#include <event_handler/event_handler.hpp>
 #include <Arduino.h>
 
 
-
-void setup() {
-    Serial.begin(115200);
-}
+void setup() {}
 
 void loop() {
-    Core.process_event(Start{});
-    delay(2000);
-    Core.process_event(Timeout{});
-    delay(2000);
-    Core.process_event(Terminate{});
-    delay(2000);
-    Core.process_event(Reset{});
-    delay(500);
+    handle_events();
 }
