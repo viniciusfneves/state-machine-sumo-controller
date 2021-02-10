8bb6f74699ba20d3d359f87784192798d8a336ad
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-02-10 15:09:10 -0300

    Reorganizando as transições da máquina - Melhora a leitura

--- Integrado/Nucleo da Simulacao/include/machine/machine.hpp
+++ Integrado/Nucleo da Simulacao/include/machine/machine.hpp
@@ -16,8 +16,8 @@ struct machine
         {
             using namespace sml;
             return make_transition_table(
-                *"initial"_s = "initialMove"_s,
-                "initialMove"_s + event<Timeout> = "SearchStrategy"_s);
+                *"initial"_s = "initialMove"_s, 
+                "initialMove"_s        +         event<Timeout>                                      =          "SearchStrategy"_s);
         }
     };
 
@@ -28,17 +28,15 @@ public:
         return make_transition_table(
             *"initial"_s = "Configuration"_s,
 
-            "Configuration"_s + event<Start> / []{Serial.println("Transiting to StartClock State");}= "StartClock"_s,
+            "Configuration"_s           +         event<Start>                                       =           "StartClock"_s,
 
-            "StartClock"_s + on_entry<_> / []{Serial.println("Entered StartClack State");},
-            "StartClock"_s + event<Timeout> / []{Serial.println("Transiting to FightState");} = state<FightState>,
-            "StartClock"_s + event<Terminate> = "DisengageRobot"_s,
+            "StartClock"_s              +         event<Timeout>                                     =           state<FightState>,
+            "StartClock"_s              +         event<Terminate>                                   =           "DisengageRobot"_s,
 
-            state<FightState> + on_entry<_> / []{Serial.println("Entered FightState");},
-            state<FightState> + event<Terminate> = "DisengageRobot"_s,
+            state<FightState>           +         on_entry<_>           /           run,
+            state<FightState>           +         event<Terminate>                                   =           "DisengageRobot"_s,
 
-            "DisengageRobot"_s + on_entry<_> / []{Serial.println("STOP");},
-            "DisengageRobot"_s + event<Reset> = "Configuration"_s);
+            "DisengageRobot"_s          +         event<Reset>                                       =           "Configuration"_s);
     };
 };
 
