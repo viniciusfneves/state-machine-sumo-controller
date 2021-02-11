ffd91747ad5f8e7d53afb90b449e9c88e45a78f3
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-02-11 15:58:06 -0300

    Estratégias vazias para movimento inicial e de busca

---
+++ Integrado/Nucleo da Simulacao/include/strategies/search_strategies/search_none.hpp
@@ -0,0 +1,22 @@
+#ifndef SEARCH_NONE_HPP
+#define SEARCH_NONE_HPP
+
+#include <sml.hpp>
+#include <event_handler/event_handler.hpp>
+
+namespace sml = boost::sml;
+
+auto exit_state = [] { setTimeoutEvent(); };
+
+struct SearchNone
+{
+    auto operator()() const
+    {
+        using namespace sml;
+        return make_transition_table(
+            "entry"_s = "switch_state"_s,
+            "switch_state"_s / exit_state = X);
+    }
+};
+
+#endif
