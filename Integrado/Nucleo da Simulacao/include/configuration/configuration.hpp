593f13da7e0cd8fe99fee460565aae528c950e5f
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-02-23 06:42:58 -0300

    Adicionando radar ao seletor de estratégias de busca

--- Integrado/Nucleo da Simulacao/include/configuration/configuration.hpp
+++ Integrado/Nucleo da Simulacao/include/configuration/configuration.hpp
@@ -8,7 +8,8 @@ enum class InitialMove
 };
 enum class Search
 {
-    none
+    none,
+    radar
 };
 
 enum class Chase
--- Integrado/Nucleo da Simulacao/include/selectors/search_selector/search_selector.hpp
+++ Integrado/Nucleo da Simulacao/include/selectors/search_selector/search_selector.hpp
@@ -4,6 +4,7 @@
 #include "../../../lib/boost/sml.hpp"
 #include "../../configuration/configuration.hpp"
 #include "../../strategies/search_strategies/search_none.hpp"
+#include "../../strategies/search_strategies/radar.hpp"
 
 namespace sml = boost::sml;
 
@@ -14,10 +15,12 @@ struct SearchSelector
         using namespace sml;
 
         auto none        = [] { return config.search == Search::none; };
+        auto radar       = [] { return config.search == Search::radar; };
 
         return make_transition_table(
             *"entry"_s = "selector"_s,
-            "selector"_s [none] = state<SearchNone>);
+            "selector"_s [none]  = state<SearchNone>,
+            "selector"_s [radar] = state<Radar>);
     }
 };
 
