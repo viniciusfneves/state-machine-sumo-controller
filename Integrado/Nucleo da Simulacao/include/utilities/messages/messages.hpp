9d447475fe43911bf9aadb3dd1b8185624b07c67
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-02-24 10:09:24 -0300

    Implementando função híbrida de prints na tela

---
+++ Integrado/Nucleo da Simulacao/include/utilities/messages/messages.hpp
@@ -0,0 +1,21 @@
+#if !defined(MESSAGES_HPP)
+#define MESSAGES_HPP
+
+#ifdef REAL_ROBOT
+#include <Arduino.h>
+#endif
+#ifndef REAL_ROBOT
+#include <iostream>
+#endif
+
+void display_message(const char *message)
+{
+#ifdef REAL_ROBOT
+    Serial.println(message);
+#endif
+#ifndef REAL_ROBOT
+    cout << message << endl;
+#endif
+};
+
+#endif // MESSAGES_HPP
