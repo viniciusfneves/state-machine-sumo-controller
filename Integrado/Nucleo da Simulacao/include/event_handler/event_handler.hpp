bd0fc2c5b0e8add51696b3cd9ae73fe11881d10c
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-02-26 09:44:48 -0300

    Disparando evento de BorderDetected dinamicamente com a leitura dos sensores

--- Integrado/Nucleo da Simulacao/include/event_handler/event_handler.hpp
+++ Integrado/Nucleo da Simulacao/include/event_handler/event_handler.hpp
@@ -3,12 +3,19 @@
 
 #include "../machine/main_machine.hpp"
 #include "../events/events.hpp"
+#include "../sensors/edge_sensor/edge_sensor.hpp"
 #include "../utilities/timeout_implementation/timeout.hpp"
+#include "../utilities/messages/messages.hpp"
 
 // Responsável por processar informações e emitir
 // eventos dinamicamente para a máquina
 void handle_events()
 {
+    if (isOnEdge())
+    {
+        display_message("On Edge");
+        Core.process_event(BorderDetected{});
+    }
     if (timeoutActive)
     {
         if (millis() >= timeoutTime)
