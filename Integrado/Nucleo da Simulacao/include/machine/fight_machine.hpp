cfa39e36cabecd4f7ecbeb4dc1f8a466ba9b30e3
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-02-26 09:39:02 -0300

    Adicionando transição para BorderDetected na FIGHT MACHINE

--- Integrado/Nucleo da Simulacao/include/machine/fight_machine.hpp
+++ Integrado/Nucleo da Simulacao/include/machine/fight_machine.hpp
@@ -5,6 +5,7 @@
 #include "../selectors/initial_move_selector/initial_move_selector.hpp"
 #include "../selectors/search_selector/search_selector.hpp"
 #include "../selectors/chase_selector/chase_selector.hpp"
+#include "../motors/drive_motors.hpp"
 
 namespace sml = boost::sml;
 
@@ -16,11 +17,17 @@ struct FightMachine
     auto operator()() const
     {
         using namespace sml;
+
+        // Funções
+        auto avoidEdge = [] {stop();drive(-120,-120);delay(200);};
+
         return make_transition_table(
-            *"entry"_s                                                            = state<InitialMoveSelector>,
-            state<InitialMoveSelector>       + event<Timeout>                     = state<SearchSelector>,
-            state<SearchSelector>            + event<OpponentDetected>            = state<ChaseSelector>,
-            state<ChaseSelector>             + event<None>                        = state<SearchSelector>);
+            *"entry"_s                                                                    = state<InitialMoveSelector>,
+            state<InitialMoveSelector>       + event<Timeout>                             = state<SearchSelector>,
+            state<InitialMoveSelector>       + event<BorderDetected>     / avoidEdge      = "AvoidEdge"_s,
+            "AvoidEdge"_s                                                                 = state<SearchSelector>,
+            state<SearchSelector>            + event<OpponentDetected>                    = state<ChaseSelector>,
+            state<ChaseSelector>             + event<None>                                = state<SearchSelector>);
     }
 };
 #endif
