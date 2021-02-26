ca2627e414d2bc80220b41ef6b3c8f40d6d11452
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-02-26 16:15:25 -0300

    Implementando lógica de leitura dos sensores de oponentes

--- Integrado/Nucleo da Simulacao/include/sensors/opponent_sensor/opponent_sensor.hpp
+++ Integrado/Nucleo da Simulacao/include/sensors/opponent_sensor/opponent_sensor.hpp
@@ -1,9 +1,49 @@
 #ifndef OPPONENT_SENSOR_HPP
 #define OPPONENT_SENSOR_HPP
 
+#define NUMBER_OF_SENSORS 5
+
+bool opponentSensorDetectionArray[NUMBER_OF_SENSORS];
+
 #ifdef REAL_ROBOT
-bool getLeftOpponentSensor(){};
-bool getRightOpponentSensor(){};
+#include "../../pins/pins.hpp"
+#ifdef SUMO3KG
+int opponentSensorWeight[5] = {-2, -1, 0, 1, 2};
+#endif
+
+int sumArray(bool vector[])
+{
+    int sum = 0;
+    for (int index = 0; index < NUMBER_OF_SENSORS; index++)
+    {
+        sum += vector[index];
+    }
+    return sum;
+};
+
+int readOpponentSensors()
+{
+    for (int index = 0; index < NUMBER_OF_SENSORS; index++)
+    {
+        opponentSensorDetectionArray[index] = digitalRead(pins::opponentsSensors::sensors[index]);
+    }
+    return sumArray(opponentSensorDetectionArray);
+};
+
+bool isOpponentDetected()
+{
+    if (readOpponentSensors() >= 1)
+    {
+        return true;
+    }
+    return false;
+};
+
+void initOpponentSensors(){
+    for (int index = 0; index < NUMBER_OF_SENSORS; index++){
+        pinMode(pins::opponentsSensors::sensors[index], INPUT);
+    }
+};
 #endif
 
 #ifndef REAL_ROBOT
