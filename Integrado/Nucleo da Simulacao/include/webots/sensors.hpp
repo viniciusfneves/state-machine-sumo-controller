d3e060c6be7930719ac64e162e962b130b368f66
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-02-27 12:50:36 -0300

    Refatorando lógica de leitura dos sensores de oponentes na simulação

--- Integrado/Nucleo da Simulacao/include/webots/sensors.hpp
+++ Integrado/Nucleo da Simulacao/include/webots/sensors.hpp
@@ -4,50 +4,17 @@
 #include "C:/Program Files/Webots/include/controller/cpp/webots/DistanceSensor.hpp"
 #include "../utilities/messages/messages.hpp"
 
+// DEFINES
+#define NUMBER_OF_OPPONENT_SENSORS 2
+
 using namespace webots;
+
+int timeStep = 0;
 
 DistanceSensor *leftEdgeSensor;
 DistanceSensor *rightEdgeSensor;
 DistanceSensor *leftDistanceSensor;
 DistanceSensor *rightDistanceSensor;
-
-int timeStep = 0;
-
-// 0=borda | 1024=semBorda
-bool getLeftEdgeSensor()
-{
-    double reading = leftEdgeSensor->getValue();
-    if (reading <= 300)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-bool getRightEdgeSensor()
-{
-    double reading = rightEdgeSensor->getValue();
-    if (reading <= 300)
-    {
-        return true;
-    }
-    else
-    {
-        return false;
-    }
-}
-
-// 0=semObjeto | 0(longe)->1060(perto)=objeto
-bool getLeftOpponentSensor()
-{
-    return leftDistanceSensor->getValue();
-}
-bool getRightOpponentSensor()
-{
-    return rightDistanceSensor->getValue();
-}
 
 // FUNÇÕES GERAIS
 void enableSensors()
@@ -80,4 +47,49 @@ void initSensors(Robot *robot, int _timeStep)
     enableSensors();
 }
 
+int sumArray(bool vector[])
+{
+    int sum = 0;
+    for (int index = 0; index < NUMBER_OF_OPPONENT_SENSORS; index++)
+    {
+        sum += vector[index];
+    }
+    return sum;
+};
+
+// SENSORES DE BORDA
+
+// 0=borda | 1024=semBorda
+bool getLeftEdgeSensor()
+{
+    double reading = leftEdgeSensor->getValue();
+    return reading < 300;
+}
+bool getRightEdgeSensor()
+{
+    double reading = rightEdgeSensor->getValue();
+    return reading < 300;
+}
+
+// SENSORES DE OPONENTE
+
+bool opponentSensorDetectionArray[NUMBER_OF_OPPONENT_SENSORS];
+int opponentSensorWeight[NUMBER_OF_OPPONENT_SENSORS] = {-1, 1};
+double error;
+
+// 0=semObjeto | 0(longe)->1060(perto)=objeto
+bool getLeftOpponentSensor()
+{
+    return leftDistanceSensor->getValue() > 0.35;
+}
+bool getRightOpponentSensor()
+{
+    return rightDistanceSensor->getValue() > 0.35;
+}
+int readOpponentSensors()
+{
+    opponentSensorDetectionArray[0] = getLeftOpponentSensor();
+    opponentSensorDetectionArray[1] = getRightOpponentSensor();
+    return sumArray(opponentSensorDetectionArray);
+};
 #endif
