6cc033f8acf5d5d884947ff3384aa44c1b7bedba
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-03-05 09:50:59 -0300

    Removendo prints de JSON na Serial

--- Integrado/Nucleo da Simulacao/include/communications/WiFi/depedencies/websockets_server/websockets_functions.hpp
+++ Integrado/Nucleo da Simulacao/include/communications/WiFi/depedencies/websockets_server/websockets_functions.hpp
@@ -8,7 +8,6 @@ void trowRobotConfigurationData()
     String payload = "{ \"robot_name\" : \"Atena\", \"configurations\" : {\"initial_move\":\"none\",\"search\":\"none\",\"chase\":\"standard\"}}";
     // READINGS , {\"readings\" : {\"opponent_sensor\": [false,false,false,false,false], \"edge_sensor\":[false, false], \"motors_pwm\":[255,255]} 
     webSocket.broadcastTXT(payload);
-    Serial.println(payload);
 }
 
 
