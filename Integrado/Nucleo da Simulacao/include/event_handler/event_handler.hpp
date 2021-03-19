ad20c5b1529fd7c6b8d0e45152cb3dc00e9568c8
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-03-19 01:56:06 -0300

    CircularBuffer agora é lidado em outro arquivo no código

--- Integrado/Nucleo da Simulacao/include/event_handler/event_handler.hpp
+++ Integrado/Nucleo da Simulacao/include/event_handler/event_handler.hpp
@@ -2,7 +2,7 @@
 #define EVENT_HANDLER_HPP
 
 #include "../machine/main_machine.hpp"
-#include "../events/events.hpp"
+#include "../event_handler/circular_buffer.hpp"
 #include "../sensors/sensors.hpp"
 #include "../utilities/timeout_implementation/timeout.hpp"
 #ifdef SUMO3KG
