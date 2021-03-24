bf6a413f3e394ff2b9f2eb2d2a37bbf946e9bae6
Vinicius F. Neves <viniciusfneves@poli.ufrj.br>
2021-03-24 04:38:14 -0300

    PID construído

--- Integrado/Nucleo da Simulacao/include/motors/PID_controller.hpp
+++ Integrado/Nucleo da Simulacao/include/motors/PID_controller.hpp
@@ -1,6 +1,24 @@
 #if !defined(PID_CONTROLLER_HPP)
 #define PID_CONTROLLER_HPP
 
+#define Kp 60
+#define Ki 1
+#define Kd 0.4
 
+double pid(double error)
+{
+    double proportional = 0, integral = 0, derivative = 0;
+    double last_error = 0, output = 0;
+
+    proportional    = Kp * error;
+    integral        = integral + Ki * error;
+    derivative      = Kd * (error - last_error);
+
+    output = proportional + integral + derivative;
+
+    last_error = error;
+
+    return output;
+}
 
 #endif // PID_CONTROLLER_HPP
