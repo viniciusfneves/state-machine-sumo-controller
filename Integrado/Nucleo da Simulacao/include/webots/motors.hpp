48b19edc424e3c7144f29956dc231e237554d16b
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-04-01 11:49:49 -0300

    Removendo função duplicada
    Também ajustando os parâmetros de velocidade da simulação

--- Integrado/Nucleo da Simulacao/include/webots/motors.hpp
+++ Integrado/Nucleo da Simulacao/include/webots/motors.hpp
@@ -3,7 +3,7 @@
 
 #include "C:/Program Files/Webots/include/controller/cpp/webots/Robot.hpp"
 #include "C:/Program Files/Webots/include/controller/cpp/webots/Motor.hpp"
-#define MAX_SPEED 250
+#define MAX_SPEED 190
 #define MAX_PWM   255
 
 using namespace webots;
@@ -45,39 +45,6 @@ void stopMotors()
     leftMotor->setVelocity(0);
     rightMotor->setVelocity(0);
 }
-
-// Movimenta o robô baseado nos parâmetros de entrada de velocidade linear e velocidade angular desejada
-// double linearSpeed -> Velocidade linear do robô -> Range [-1,1]
-// double angularSpeed -> Velocidade angular do robô -> Range [-1,1]
-void driveRobot(int linearSpeed, int angularSpeed)
-{
-    // Limita os parâmetros de entrada aos permitidos pela função
-    // Só funciona se os parametros forem de -1 à 1, caso contrário utilize um MAP para definir as velocidas linear e angular
-    linearSpeed = constrain(linearSpeed, -1, 1) * robotSpecifications.maxLinearSpeed;
-    angularSpeed = constrain(angularSpeed, -1, 1) * robotSpecifications.maxAngularSpeed;
-
-    // Transforma os parâmetros de velocidade linear e angular em potência para os motores
-    int PWM_left = (2 * linearSpeed - angularSpeed * (robotSpecifications.wheelBase)) / (2 * (robotSpecifications.wheelRadius)) * 255;
-    int PWM_right = (2 * linearSpeed + angularSpeed * (robotSpecifications.wheelBase)) / (2 * (robotSpecifications.wheelRadius)) * 255;
-
-    //Assegura que a velocidade angular vai ser exercida como pedido, podendo alterar a velocidade linear para isso
-    double maxSpeed = (PWM_left > PWM_right) ? PWM_left : PWM_right;
-    double minSpeed = (PWM_left < PWM_right) ? PWM_left : PWM_right;
-
-    if (maxSpeed > robotConfiguration.maxSpeed)
-    {
-        PWM_left -= maxSpeed - robotConfiguration.maxSpeed;
-        PWM_right -= maxSpeed - robotConfiguration.maxSpeed;
-    }
-    else if (minSpeed < -robotConfiguration.maxSpeed)
-    {
-        PWM_left -= minSpeed + robotConfiguration.maxSpeed;
-        PWM_right -= minSpeed + robotConfiguration.maxSpeed;
-    }
-
-    // Comanda os motores à rodar com as potências calculadas
-    driveMotors(PWM_left, PWM_right);
-};
 
 Motor *initMotor(Robot *robot, char *motorName)
 {
