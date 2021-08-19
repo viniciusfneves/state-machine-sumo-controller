e3cc6aa0056fed9977a695167ee1101cce3a8a05
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-08-19 14:38:12 -0300

    Optimizando definições de especificações em todos os ambientes
    - Agora cada ambiente possui seu próprio número de sensores, nome, tamanho da base e raio de roda
    
    - Todos os ambientes possuem em comum os cálculos de velocidade linear e angular máximas
    
    - Maior organização e separação entre ambientes
    
    - Nada alterado no env de simulação
    
    - Alterado quantidade de sensores de oponentes do MERI para 5

--- Integrado/Nucleo/include/configuration/specifications.hpp
+++ Integrado/Nucleo/include/configuration/specifications.hpp
@@ -6,85 +6,70 @@
 /* ---> PRECISA ESTAR DEFINIDO INDEPENDENTE DO AMBIENTE <--- */
 #define NUMBER_OF_CONTROLLER_CHANNELS 2
 
-// Especificações dos robôs 3Kg
+struct Specifications
+{
+    // 3KG //
+
 #ifdef SUMO3KG
+    // Define o nome do robo
+    String robotName = "Atena";
+    // Quantidade de sensores de cada categoria (Borda e Oponente)
 #define NUMBER_OF_EDGE_SENSORS 2
 #define NUMBER_OF_OPPONENT_SENSORS 5
+    // Especificações mecânicas do robô necessárias para alguns algoritmos do código
+    const int wheelBase = 132;           // Distância entre as rodas em milímetros
+    const double wheelRadius = 72.5 / 2; // Raio das rodas em milímetros
 #endif
 
-// Especificações dos robôs Minis
-#ifdef SUMOMINI
+    // MINIS //
+
+#ifdef MERI
+    // Define o nome do robo
+    String robotName = "Meri";
+    // Quantidade de sensores de cada categoria (Borda e Oponente)
 #define NUMBER_OF_EDGE_SENSORS 2
 #define NUMBER_OF_OPPONENT_SENSORS 2
+    // Especificações mecânicas do robô necessárias para alguns algoritmos do código
+    const int wheelBase = 75.2;        // Distância entre as rodas em milímetros. Considerando o centro da roda
+    const double wheelRadius = 34 / 2; // Raio das rodas em milímetros
 #endif
 
+#ifdef ET_MINI
+    // Define o nome do robô
+    String robotName = "ET";
+    // Quantidade de sensores de cada categoria (Borda e Oponente)
+#define NUMBER_OF_EDGE_SENSORS 2
+#define NUMBER_OF_OPPONENT_SENSORS 2
+    // Especificações mecânicas do robô necessárias para alguns algoritmos do código
+    const int wheelBase = 75;          // Distância entre as rodas em milímetros. Considerando o centro da roda
+    const double wheelRadius = 34 / 2; // Raio das rodas em milímetros
+#endif
+
+#ifdef ZE_PEQUENO
+    // Define o nome do robo
+    String robotName = "Ze";
+    // Quantidade de sensores de cada categoria (Borda e Oponente)
+#define NUMBER_OF_EDGE_SENSORS 2
+#define NUMBER_OF_OPPONENT_SENSORS 5
+    // Especificações mecânicas do robô necessárias para alguns algoritmos do código
+    const int wheelBase = 75;          // Distância entre as rodas em milímetros. Considerando o centro da roda
+    const double wheelRadius = 34 / 2; // Raio das rodas em milímetros
+#endif
+
+    // GERAL //
+
+    const double maxLinearSpeed = wheelRadius;
+    const double maxAngularSpeed = 2 * wheelRadius / wheelBase;
+};
+
+// Objeto de especificações do robô
+Specifications robotSpecifications;
+
+// SIMULAÇÃO //
 // Especificações da simulação com Webots
 #ifdef WEBOTS
 #define NUMBER_OF_EDGE_SENSORS 2
 #define NUMBER_OF_OPPONENT_SENSORS 2
 #endif
-// Sumo 3kg //
-
-#ifdef SUMO3KG
-struct Specifications
-{
-    // Define o nome do robo
-    String robotName = "Atena";
-
-    // Especificações mecânicas do robô necessárias para alguns algoritmos do código
-    const int wheelBase = 132;           // Distância entre as rodas em milímetros
-    const double wheelRadius = 72.5 / 2; // Raio das rodas em milímetros
-    const double maxLinearSpeed = wheelRadius;
-    const double maxAngularSpeed = 2 * wheelRadius / wheelBase;
-};
-#endif
-
-// Minis //
-
-#ifdef MERI
-struct Specifications
-{
-    // Define o nome do robo
-    String robotName = "Meri";
-    
-    // Especificações mecânicas do robô necessárias para alguns algoritmos do código
-    const int wheelBase = 75.2;           // Distância entre as rodas em milímetros. Considerando o centro da roda
-    const double wheelRadius = 34 / 2; // Raio das rodas em milímetros
-    const double maxLinearSpeed = wheelRadius;
-    const double maxAngularSpeed = 2 * wheelRadius / wheelBase;
-};
-#endif
-
-
-#ifdef ET_MINI
-struct Specifications
-{
-    // Define o nome do robô
-    String robotName = "ET";
-
-    // Especificações mecânicas do robô necessárias para alguns algoritmos do código
-    const int wheelBase = 75;           // Distância entre as rodas em milímetros. Considerando o centro da roda
-    const double wheelRadius = 34 / 2; // Raio das rodas em milímetros
-    const double maxLinearSpeed = wheelRadius;
-    const double maxAngularSpeed = 2 * wheelRadius / wheelBase;
-};
-#endif
-
-#ifdef ZE_PEQUENO
-struct Specifications
-{
-    // Define o nome do robo
-    String robotName = "Ze";
-
-    // Especificações mecânicas do robô necessárias para alguns algoritmos do código
-    const int wheelBase = 75;           // Distância entre as rodas em milímetros. Considerando o centro da roda
-    const double wheelRadius = 34 / 2; // Raio das rodas em milímetros
-    const double maxLinearSpeed = wheelRadius;
-    const double maxAngularSpeed = 2 * wheelRadius / wheelBase;
-};
-#endif
-
-// Objeto de especificações do robô
-Specifications robotSpecifications;
 
 #endif // SPECIFICATIONS_HPP
