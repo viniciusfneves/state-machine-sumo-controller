32aff5e81cc423e458fee2483774bf1790ee2c01
jomarjunior <jomarjunior@poli.ufrj.br>
2021-09-15 17:15:50 -0300

    alterações para funcionar no webots

--- Integrado/Nucleo/include/strategies/search_strategies/radar.hpp
+++ Integrado/Nucleo/include/strategies/search_strategies/radar.hpp
@@ -4,7 +4,11 @@
 #include "../../../lib/boost/sml.hpp"
 #include "../../motors/drive_motors.hpp"
 
+#include <iostream>
+
 namespace sml = boost::sml;
+
+#define tal 100
 
 struct SearchRadar
 {
@@ -15,9 +19,22 @@ struct SearchRadar
         auto left  = [] { return true; };  // Implementar função para verificar qual o último lado visto pelos sensores de oponentes
         auto right = [] { return false; }; // Implementar função para verificar qual o último lado visto pelos sensores de oponentes
 
-        // Funções
-        auto rotate_left  = [] { driveRobot(0,-1); };
-        auto rotate_right = [] { driveRobot(0,1); };
+        // Funções MINI
+        auto rotate_left = []
+        {
+            static double tRadar1 = millis();
+            driveRobot(
+                0,
+                -1 + exp(-(millis() - tRadar1) * tal));
+        };
+        auto rotate_right = []
+        {
+            static double tRadar2 = millis();
+            driveRobot(
+                0,
+                1 - exp(-(millis() - tRadar2) * tal));
+        };
+
 
         return make_transition_table(
             *"entry"_s = "switch"_s,
@@ -26,7 +43,10 @@ struct SearchRadar
             "switch"_s  [right]  = "rotate_right"_s,
 
             "rotate_left"_s   + on_entry<_>  / rotate_left,
-            "rotate_right"_s  + on_entry<_>  / rotate_right);
+            "rotate_left"_s   = "switch"_s,
+            "rotate_right"_s  + on_entry<_>  / rotate_right,
+            "rotate_right"_s  = "switch"_s
+        );
     }
 };
 #endif
--- Integrado/Simulacoes/Geral/controllers/state_machine_auto_controller/state_machine_auto_controller.cpp
+++ Integrado/Simulacoes/Geral/controllers/state_machine_auto_controller/state_machine_auto_controller.cpp
@@ -6,7 +6,7 @@
 #include "../../../../Nucleo/include/event_handler/event_handler.hpp"
 #include "../../../../Nucleo/include/events/events.hpp"
 
-#define TIME_STEP 1
+#define TIME_STEP 60
 
 using namespace webots;
 
