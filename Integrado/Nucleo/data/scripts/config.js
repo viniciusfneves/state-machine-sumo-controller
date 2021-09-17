60a3cabf757fa119c06a3c256a186947e4b40fe8
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-09-17 18:25:32 -0300

    enviando comando para mudança de parametros ao robô

--- Integrado/Nucleo/data/scripts/config.js
+++ Integrado/Nucleo/data/scripts/config.js
@@ -153,6 +153,16 @@ function setStrategy(strategyType, strategy) {
 	connection.send('{ "' + type + '" : "' + strategy + '" }');
 }
 
+// Envia um request pro robô para alterar sua configuração de parâmetros de start_time e constantes do PID
+// Pega os valores automaticamente dos campos da página de configurações
+function setParameters(){
+    let parameter_start_time = document.getElementById("start-time-value").value;
+    let parameter_pid_kp = document.getElementById("P-regulator-value").value;
+    let parameter_pid_ki = document.getElementById("I-regulator-value").value;
+    let parameter_pid_kd = document.getElementById("D-regulator-value").value;
+    connection.send('{"start_time":' + parameter_start_time + ',"pid":{"kp":' + parameter_pid_kp + ',"ki":' + parameter_pid_ki + ',"kd":' + parameter_pid_kd + '}}');
+}
+
 //Envia um request para a máquina do robô emitir um evento específico
 function requestEvent(event) {
 	connection.send('{ "event_request" : "' + event + '" }');
