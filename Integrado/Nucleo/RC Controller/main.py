85f008f07e39ce18801e5260b392632fae28841d
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2021-09-23 17:18:18 -0300

    Adaptando programa para envio dos comandos RC
    Adaptando arquivo python para enviar as informações do controle usando a nova lógica do robô, mais simplificada e que evita erros usando JSON no corpo da mensagem

--- Integrado/Nucleo/RC Controller/main.py
+++ Integrado/Nucleo/RC Controller/main.py
@@ -5,7 +5,7 @@ import time
 # Define de quanto em quanto tempo os comandos devem ser atualizados no robô
 taxa_de_atualizacao = 30    # Em milissegundos
 
-payload = {"linear": 0, "angular": 0}
+payload = {"controller":{"linearSpeed": 0, "angularSpeed": 0}}
 
 def initMessage():
     print("Conectando")
@@ -33,8 +33,8 @@ def main():
             calibrated = True
             print("Calibrado!\nEnviando comandos...")
 
-        payload["linear"] = controller.linearSpeed
-        payload["angular"] = controller.angularSpeed
+        payload["controller"]["linearSpeed"] = controller.linearSpeed
+        payload["controller"]["angularSpeed"] = controller.angularSpeed
 
         # Envia as informações para o robô via http
         status = send_commands(payload)
@@ -46,8 +46,8 @@ def main():
         # Botão Start encerra o programa e envia comando para parar os motores
         if controller.Start == 1:
             print("Encerrando robô...")
-            payload["linear"] = 0
-            payload["angular"] = 0
+            payload["controller"]["linearSpeed"] = 0
+            payload["controller"]["angularSpeed"] = 0
             # Continua enviando sinal de parada dos motores até que recebe uma confirmação do robô que o sinal foi interpretado corretamente
             while (send_commands(payload) == False):
                 pass
--- Integrado/Nucleo/RC Controller/send_commands.py
+++ Integrado/Nucleo/RC Controller/send_commands.py
@@ -3,7 +3,7 @@ import requests
 # Envia um request de POST no endereço do ESP32 responsável pelo recebimento dos comandos do controle
 # Retorna True caso o recebimento tenha sido bem sucedido e False caso tenha ocorrido algum erro
 def send_commands(payload):
-    r = requests.post("http://192.168.4.1/controller", data=payload)
+    r = requests.post("http://192.168.4.1/controller", json=payload)
     if r.status_code != 200:
         return False
     return True
