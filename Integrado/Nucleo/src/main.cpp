74bba500113c14433f128ac1654ee7e44333e249
Vinícius F Neves <viniciusfneves@poli.ufrj.br>
2022-02-06 18:53:35 -0300

    Removendo serverHTTP e atualizando comentários sobre o controle de PS4

--- Integrado/Nucleo/src/main.cpp
+++ Integrado/Nucleo/src/main.cpp
@@ -10,32 +10,30 @@
 #ifdef ESP32_ENV
 #include <communications/PS4_controller/ps4_controller.hpp>
 #include <communications/WiFi/WiFi.hpp>
-#include <communications/WiFi/http_server/http_server.hpp>
 #include <communications/WiFi/websockets_server/websockets_handlers.hpp>
 #endif
 
 void setup() {
 // Configurações do ambiente para Arduino
 #ifdef ARDUINO_ENV
-    // Serial.begin(9600);
+    Serial.begin(9600);
 #endif
 
 // Configurações do ambiente para ESP32
 #ifdef ESP32_ENV
     // ---- AVISO PARA USO DO CONTROLE DE PS4 ---- //
-    // O uso do controle juntamente com os WebServers só pode ser feito utilizando uma rede WiFi externa
-    // Ao tentar utilizar o ESP32 como AcessPoint e conectar e ler o controle, o AcessPoint é terminado e portanto não será
-    // mais possível se conectar aos servers remotamente
+    // Ao tentar utilizar o ESP32 como AcessPoint Wi-Fi e BLE para se comunicar com o controle,
+    // o AcessPoint é terminado e o robô para de responder
     // ------------------------------------------ // ------------------------------------------ //
     // Para usar todas as features do Mesin Negara, comente "initAcessPoint()" e utilize "initController()"
     // junto com "connectToWiFi()"fornecendo à função o SSID e a senha da rede que deseja se conectar
-    // Futuramente: Será implementado um serviço mDNS para facilitar a conexão via redes externas onde o IP é dinâmico
     // ------------------------------------------ // ------------------------------------------ //
-    initController();
-    // connectToWiFi("SSID", "PASSWORD");
+    // Se quiser utilizar o ESP como AcessPoint, comente "initController()" e tudo funcionará, exceto o controle de PS4
 
     // initAccessPoint();
-    // initHTTPServer();
+    // connectToWiFi("SSID", "PASSWORD");
+
+    initController();
     initWebSocketsServer();
     performance.setStackSize(CONFIG_ARDUINO_LOOP_STACK_SIZE);
 #endif
